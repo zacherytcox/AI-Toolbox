05250c966680c523bc8a45d062627c47ec7763b3
Zachery Cox <zacherycox@zacherys.macbook.pro.home>
2025-11-19 21:16:34 -0500

    Testing k8s

--- run_microk8s.sh
+++ run_microk8s.sh
@@ -3,6 +3,8 @@
 
 docker build . -t localhost:32000/ai-toolbox:latest
 
+docker push localhost:32000/ai-toolbox
+
 # microk8s ctr image pull microk8s-1:32000/ai-toolbox:latest
 # microk8s kubectl create deployment nginx --image=10.141.241.175:32000/mynginx:registry
 # microk8s images import microk8s-1:32000/ai-toolbox:latest
